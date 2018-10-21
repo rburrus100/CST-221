cd8c02880e6c722a6d6599fb1b19f8991b9f958e
rburrus100 <43736337+rburrus100@users.noreply.github.com>
2018-10-21 14:54:10 -0500

    Update converter.c

--- converter.c
+++ converter.c
@@ -2,13 +2,14 @@
  
 int main()
 {
-  int n, c, k;
+  int n, c, k, remainder, quotient, j = 0;
+  char hexadecimal[100];
  
   printf("Enter an integer in decimal number system\n");
   scanf("%d", &n);
  
   printf("%d in binary number system is:\n", n);
- 
+ //code to convert decimal to bianary
   for (c = 31; c >= 0; c--)
   {
     k = n >> c;
@@ -21,7 +22,26 @@ int main()
  
   printf("\n");
  
- scanf("%d",&n);
-	printf("Equivalent hexadecimal number is: %X",n);
-	return 0;
+ scanf("%ld", &n);
+ printf("%ld \n",n);
+ 
+ quotient = n;
+ 
+ while (quotient != 0)
+ {
+ remainder = quotient % 16;          //step 1
+ if (remainder < 10)
+ hexadecimal[j++] = 48 + remainder;   //step 2
+ else
+ hexadecimal[j++] = 55 + remainder;   //step 3
+ quotient = quotient / 16;            //step 4
+ }
+ 
+ printf("Equivalent hexadecimal value of decimal number %ld:",decimal);
+ 
+ //step 6
+ for (i = j; i >= 0; i--)
+ printf("%c", hexadecimal[i]);
+ return 0;
 }
+
